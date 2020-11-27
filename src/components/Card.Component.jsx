6f29cf0d0ec307473128a5a8aadbb521332f6ff2
bassam <n7520@student.jamk.fi>
2020-11-27 16:57:26 +0200

    27-11-update-3

--- src/components/Card.Component.jsx
+++ src/components/Card.Component.jsx
@@ -20,15 +20,15 @@ const Card = () => {
                   <p className="clickable">
                   This is was made as assignment for YTSP0300 - Application Frameworks course by Bassam AlAsadi.
                   </p> 
-                  <h3>Documentation</h3>
+                  <h3>Documentations</h3>
                   <div className="icon-block center clickable">
+                  Source Code &nbsp;&nbsp;
                     <a href="//github.com/bassamalasadi/weatherapp/tree/master" target="_blank">
                       <FontAwesomeIcon size='3x' icon={ faGithub} />
                     </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <a href="//github.com/bassamalasadi/weatherapp/blob/master/Bassam_Alasadi_stack_research_YTSP0300.pdf" target="_blank">
-                      <FontAwesomeIcon size='3x' icon={ faFile } />
-                      
-                    </a>
+                      <FontAwesomeIcon size='3x' icon={ faFile } /> 
+                    </a>&nbsp;&nbsp; Stack Research 
                   </div>
                 </div>
               </div>
